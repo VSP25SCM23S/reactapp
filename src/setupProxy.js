ebb50b06138073c822e7a17423a9c1c2eb8c808d
Vishnu Priyan S S <vishnu32510@gmail.com>
2025-04-24 16:09:03 -0500

    update code

---
+++ src/setupProxy.js
@@ -0,0 +1,18 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+/*
+This acts a proxy between the react application and the flask microservice
+Everytime there is a request to /api, the setupProxy prepends the flask
+microservice url mentioned in line 14
+*/
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      // update the flask Google Cloud url
+      // target: "https://flaskapp-619095842085.us-central1.run.app",
+      target: "http://localhost:8080",
+      changeOrigin: true,
+    })
+  );
+};
